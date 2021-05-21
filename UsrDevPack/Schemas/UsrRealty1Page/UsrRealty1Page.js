8bcc2fa460002691ce1a2b808618e35bfd29060a
Alexnikz1 <niko-ale@yandex.ru>
2021-05-21 10:57:20 +0300

    Добавление кнопки

--- UsrDevPack/Schemas/UsrRealty1Page/UsrRealty1Page.js
+++ UsrDevPack/Schemas/UsrRealty1Page/UsrRealty1Page.js
@@ -11,12 +11,66 @@ define("UsrRealty1Page", [], function() {
 					"masterColumn": "Id",
 					"detailColumn": "UsrRealty"
 				}
+			},
+			"UsrRealtyVisitDetail": {
+				"schemaName": "UsrRealtyVisitDetail",
+				"entitySchemaName": "UsrRealtyVisit",
+				"filter": {
+					"detailColumn": "UsrRealty",
+					"masterColumn": "Id"
+				}
 			}
 		}/**SCHEMA_DETAILS*/,
 		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
-		methods: {},
+		methods: {
+			onMyButtonClick: function() {
+				this.console.log("Наша кнопка работает!");
+			},
+			getMyButtonEnabled: function() {
+				var result = true;
+				var name = this.get("UsrName");
+				if (!name) {
+					result = false;
+				}
+				
+				return result;
+			}
+	
+		},
 		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
+			// Метаданные для добавления на страницу пользовательской кнопки.
+            {
+                // Выполняется операция добавления компонента на страницу.
+                "operation": "insert",
+                // Мета-имя родительского контейнера, в который добавляется кнопка.
+                "parentName": "ProfileContainer",
+                // Кнопка добавляется в коллекцию компонентов
+                // родительского элемента.
+                "propertyName": "items",
+                // Мета-имя добавляемой кнопки.
+                "name": "MyButton",
+                // Свойства, передаваемые в конструктор компонента.
+                "values": {
+                   // Тип добавляемого элемента — кнопка.
+                    itemType: Terrasoft.ViewItemType.BUTTON,
+                    // Привязка заголовка кнопки к локализуемой строке схемы.
+                    caption: {bindTo: "Resources.Strings.MyButtonCaption"},
+                    // Привязка метода-обработчика нажатия кнопки.
+                    click: {bindTo: "onMyButtonClick"},
+                    // Привязка свойства доступности кнопки.
+                    enabled: {bindTo: "getMyButtonEnabled"},
+                    // Стиль отображения кнопки.
+                    "style": Terrasoft.controls.ButtonEnums.style.BLUE,
+					"layout": {
+						"colSpan": 15,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3,
+						"layoutName": "ProfileContainer"
+					},
+                }
+            },
 			{
 				"operation": "insert",
 				"name": "UsrName2bb2d364-d18f-4f6b-8e74-a66c3fde5e05",
@@ -129,10 +183,10 @@ define("UsrRealty1Page", [], function() {
 			},
 			{
 				"operation": "insert",
-				"name": "NotesAndFilesTab",
+				"name": "Tabafe501d3TabLabel",
 				"values": {
 					"caption": {
-						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+						"bindTo": "Resources.Strings.Tabafe501d3TabLabelTabCaption"
 					},
 					"items": [],
 					"order": 0
@@ -140,6 +194,31 @@ define("UsrRealty1Page", [], function() {
 				"parentName": "Tabs",
 				"propertyName": "tabs",
 				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrRealtyVisitDetail",
+				"values": {
+					"itemType": 2,
+					"markerValue": "added-detail"
+				},
+				"parentName": "Tabafe501d3TabLabel",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": [],
+					"order": 1
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 1
 			},
 			{
 				"operation": "insert",
@@ -197,7 +276,7 @@ define("UsrRealty1Page", [], function() {
 				"operation": "merge",
 				"name": "ESNTab",
 				"values": {
-					"order": 1
+					"order": 2
 				}
 			}
 		]/**SCHEMA_DIFF*/
---
+++ UsrDevPack/Schemas/UsrRealtyVisitPage/UsrRealtyVisitPage.js
@@ -0,0 +1,87 @@
+define("UsrRealtyVisitPage", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyVisit",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName1e8a13dd-b245-44a8-9af9-7b598b32dfc2",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrName",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "DATETIMEd8fbf099-9fbe-44ed-b163-638ef78c9d1f",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrDate",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP70410552-9320-4dde-8cfb-aa5e732aa9a2",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrVisitor",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUP061762d9-00ac-440f-b43d-27584dc089cf",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrOwner",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
